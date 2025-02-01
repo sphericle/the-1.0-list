66311d2bedd36ef25c388f2eca06c95d75288593
sphericle <86196223+sphericle@users.noreply.github.com>
2025-01-31 20:44:53 -0500

    first commit

---
+++ js/routes.js
@@ -0,0 +1,25 @@
+import List from './pages/List.js';
+import Leaderboard from './pages/Leaderboard.js';
+import Roulette from './pages/Roulette.js';
+import Packs from './pages/Packs.js';
+import ChallengeList from './Archived/ChallengeList.js';
+import ChallengeLeaderboard from './Archived/ChallengeLeaderboard.js';
+
+export default [
+    // Sets the browser link to access each page
+    { path: "/", component: List },
+    { path: '/level/:level', component: List },
+    
+    
+    { path: "/challenges", component: ChallengeList },
+
+    { path: "/leaderboard", component: Leaderboard },
+    { path: "/leaderboard/user/:user", component: Leaderboard },
+
+    { path: "/challengeleaderboard", component: ChallengeLeaderboard },
+
+    { path: "/roulette", component: Roulette },
+
+    { path: "/packs", component: Packs },
+    { path: "/packs/pack/:pack", component: Packs },
+];
